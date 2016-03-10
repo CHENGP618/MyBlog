26d24f2b3d13b50aacaf4a586ddef36ef0e7aab9
CHENG618 <chengp618@gmail.com>
2016-03-10 15:18:22 +0100

    add test mailer-mailtrap-to configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,15 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '831b25c7c14af5',
+    :password => 'c5709196cb4656',
+    :address => 'mailtrap.io',
+    :domain => 'mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
   # Do not eager load code on boot.
   config.eager_load = false
 
