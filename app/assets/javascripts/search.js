05853e2e03aabdf03adb2f339a4cb446e24b1e68
CHENG618 <chengp618@gmail.com>
2016-04-06 16:39:52 +0200

    refactoring

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,26 +1,18 @@
 $(document).ready(function () {
   $("#search").autocomplete({
-    // serviceUrl: "/articles/suggestion",
     lookup: function (query, done) {
       var ajax_query = "/articles/suggestion?query=".concat(query);
         $.ajax( ajax_query , {
           success: function (data) {
-           var results = { suggestions: []  };
+            var results = { suggestions: [] };
             $.each(data.suggestions, function (index, element) {
-              // suggestions: [{value: "in progress", data: "fds"}]
-              results.suggestions.push({value: element.title, data: element.id})
-              console.log(element);
+              results.suggestions.push({ value: element.title, data: element.id })
             });
-            console.log("success !", results);
             done(results);
           }
         } );
-        console.log("ajaxa sent");
-        // done(results);
     },
-    // minChars: 3,
     onSelect: function(suggestion){
-      // console.log(arguments.toString())
       window.location = "/articles/"+suggestion.data;
     }
   });
